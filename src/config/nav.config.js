2e42ad08fb12dd08af536c60380ccb29f0d49d66
japson <mr_japson@163.com>
2019-05-15 14:31:13 +0800

    del

--- src/config/nav.config.js
+++ src/config/nav.config.js
@@ -1,14 +1,14 @@
 export const navList = [
   {
     "labelName": "示例页面",
-    "icon": "http://officalcms.upy.jiedaibao.com/2019/0507/20190507053148764.png",
+    "icon": "",
     "toUrl": "/demo",
     "description": "这是个示例页面",
     "children": []
   },
   {
     "labelName": "随机选人系统",
-    "icon": "http://officalcms.upy.jiedaibao.com/2019/0507/20190507053148764.png",
+    "icon": "",
     "toUrl": "/cr-choose-random",
     "description": "用来随机抽取 CR 参与人员",
     "children": []
